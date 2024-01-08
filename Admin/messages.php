874797d6e61849d88a2133e4d0ce621d62f1652a
AliBaig102 <baiga8424@gmail.com>
2024-01-08 14:43:11 +0500

    add messages files

--- Admin/messages.php
+++ Admin/messages.php
@@ -8,7 +8,33 @@ require "Layout/sidebar.php";
     <div class="products_container">
         <h1>Messages List</h1>
         <div class="messages_container">
-
+            <div class="products_list_container">
+                <div class="product_list">
+                    <div>
+                        <img src="https://images.unsplash.com/photo-1441986300917-64674bd600d8?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="">
+                    </div>
+                    <h3>Product Title</h3>
+                </div>
+            </div>
+            <div class="message_box_container">
+                <div class="message_box">
+                    <div class="message_head">
+                        <img src="https://images.unsplash.com/photo-1441986300917-64674bd600d8?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="">
+                        <h1>Product Title</h1>  
+                    </div>
+                    <div class="message_body">
+                        <div>
+                            <div>
+                                <img src="https://images.unsplash.com/photo-1441986300917-64674bd600d8?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="">
+                                <h4>username</h4>
+                            </div>
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
 
     </div>
